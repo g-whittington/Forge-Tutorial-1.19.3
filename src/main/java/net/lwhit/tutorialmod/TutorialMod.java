56e66d1e0a6aaccf4b0e6f2929496fb2faf73a33
George <lwhittington2231@gmail.com>
2023-05-15 23:33:20 -0400

    Added some example change

--- src/main/java/net/lwhit/tutorialmod/TutorialMod.java
+++ src/main/java/net/lwhit/tutorialmod/TutorialMod.java
@@ -12,6 +12,8 @@ import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import org.slf4j.Logger;
 
+// Example commit from IntelliJ
+
 // The value here should match an entry in the META-INF/mods.toml file
 @Mod(TutorialMod.MOD_ID)
 public class TutorialMod {
